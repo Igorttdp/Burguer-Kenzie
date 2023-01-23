cdcfa6490dfdd3675fa025f5b14d12ad87bec1ee
Igorttdp <igorttdp.dev@email.com>
2023-01-23 12:18:26 -0300

    feat: add new styled-component

---
+++ src/components/MainDiv/MainDiv.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+  min-height: 110vh;
+  max-width: 90%;
+  width: 100%;
+  margin: 2rem auto 4rem;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  align-items: center;
+  gap: 3rem;
+
+  @media (min-width: 1400px) {
+    max-width: 1400px;
+  }
+
+  @media (min-width: 1200px) {
+    flex-flow: row nowrap;
+    align-items: flex-start;
+    min-height: fit-content;
+  }
+`;
