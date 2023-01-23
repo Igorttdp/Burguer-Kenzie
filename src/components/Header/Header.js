dd3681c6db1046ce2421f9ea948b95d21dd455ce
Igorttdp <igorttdp.dev@email.com>
2023-01-23 12:17:43 -0300

    feat: add new styled-components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  background-color: var(--grey-0);
+  padding: 1.5rem 0;
+
+  @media (min-width: 1400px) {
+    div.header__container {
+      max-width: 1400px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    div.header__container {
+      flex-flow: row nowrap;
+      justify-content: space-between;
+
+      .header__search {
+        max-width: 36.5rem;
+        margin: unset;
+      }
+    }
+  }
+
+  .header__container {
+    max-width: 90%;
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 1.4rem;
+
+    .header__logo {
+      align-self: center;
+
+      img {
+        width: 160px;
+      }
+    }
+
+    .header__search {
+      position: relative;
+      max-width: 90%;
+      width: 100%;
+      margin: 0 auto;
+
+      input {
+        width: 100%;
+        background-color: #fff;
+        border: 2px solid var(--grey-20);
+        font-size: 1.6rem;
+        padding: 2rem 11.7rem 2rem 1.5rem;
+        border-radius: 8px;
+
+        ::placeholder {
+          color: var(--grey-20);
+        }
+      }
+
+      button {
+        font-weight: 500;
+        position: absolute;
+        top: calc(2.2rem / 2);
+        right: 1rem;
+      }
+    }
+  }
+`;
