542efcd7eaa68c6e58445c792800366c24c36dc9
Igorttdp <igorttdp.dev@email.com>
2023-01-24 14:42:23 -0300

    feat: call products context for use

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,32 +1,12 @@
-const Products = ({
-  products,
-  filteredProducts,
-  cartProducts,
-  setCartProducts,
-}) => {
-  const addProducts = (e) => {
-    const el = e.target;
+import { useContext } from "react";
+import { ProductsContext } from "../../context/ProductsContext";
+import { SearchContext } from "../../context/SearchContext";
 
-    const productId = Number(
-      el.parentElement.parentElement.getAttribute("aria-labelledby")
-    );
+const Products = () => {
+  const { products, addProducts } = useContext(ProductsContext);
+  const { filteredProducts } = useContext(SearchContext);
 
-    const [ newProduct ] = products.filter((el) => el.id === productId);
-    if (!newProduct.amount) newProduct.amount = 1
-    if (!newProduct.totalPrice) newProduct.totalPrice = newProduct.price
-
-    const verify = cartProducts.some((el) => el.id === newProduct.id);
-
-    if (!verify) {
-      setCartProducts([...cartProducts, newProduct]);
-    } else {
-      newProduct.totalPrice += newProduct.price
-      newProduct.amount++
-      setCartProducts([...cartProducts]);
-    }
-  };
-
-  const arr = filteredProducts.length !== 0 ? filteredProducts : products
+  const arr = filteredProducts.length !== 0 ? filteredProducts : products;
 
   return (
     <>
