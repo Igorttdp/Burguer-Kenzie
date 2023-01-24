37671b9b0c7c990548d91bc14c1d0692a2318a11
Igorttdp <igorttdp.dev@email.com>
2023-01-24 14:33:37 -0300

    feat: add new styled-component

---
+++ src/components/AmountController/AmountControllerContainer.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+const AmountControllerContainer = styled.table`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 82.69px;
+
+  tr {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+
+    td {
+      button {
+        all: unset;
+        cursor: pointer;
+        width: 20px;
+        height: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: all 0.2s;
+      }
+
+      :nth-child(1) {
+        button {
+          border: 1px solid var(--negative);
+        }
+
+        :hover {
+          color: #fff;
+          background-color: var(--negative);
+        }
+      }
+
+      :last-child {
+        button {
+          border: 1px solid var(--color-primary);
+
+          :hover {
+            color: #fff;
+            background-color: var(--color-primary);
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default AmountControllerContainer;
