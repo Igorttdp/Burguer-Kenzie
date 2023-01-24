e7152b3f731c9fd66b09151d5ab2bf77267d541e
Igorttdp <igorttdp.dev@email.com>
2023-01-24 14:50:54 -0300

    feat: add Providers in App Component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import GlobalStyle from "./global.js";
 import App from "./App";
+import Providers from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Providers>
+      <GlobalStyle />
+      <App />
+    </Providers>
   </React.StrictMode>
 );
