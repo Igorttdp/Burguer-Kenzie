64c83293e5c4a3d60f7d7c07a83348e52e148eda
Igorttdp <igorttdp.dev@email.com>
2023-01-24 14:49:31 -0300

    feat: add theme for styles purpouses | add invertTheme function to invert theme | add ThemeProvider component to introduze darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,75 @@
-// React Hooks
-import { useState } from "react";
-import { useEffect } from "react";
-
 // Components
 import Cart from "./components/Cart";
 import Products from "./components/Products";
 import Results from "./components/Results";
+import HamburguerHeader from "./components/Header";
 
 // Styled-Components
 import { SearchResults } from "./components/Results/SearchResults";
 import { ProductCard } from "./components/Products/Products";
-import { CartContainer } from "./components/Cart/Cart";
 import { MainDiv } from "./components/MainDiv/MainDiv";
-
-// Api
-import api from "./services/api";
-import HamburguerHeader from "./components/Header";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+import AppContainer from "./components/AppContainer";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [search, setSearch] = useState("");
-  const [cartProducts, setCartProducts] = useState([]);
-  const [total, setTotal] = useState(0);
+  const theme = {
+    lightBgPrimary: "#ffffff",
+    darkBgPrimary: "#1e201f",
+    lightBgSecundary: "#F5F5F5",
+    darkBgSecundary: "#181a18",
+    lightColor: "#000",
+    darkColor: "#fff",
+    lightRemoveAllBtn: "#E0E0E0",
+    darkRemoveAllBtn: "transparent",
+    lightBorderRemoveAllBtn: "transparent",
+    darkBorderRemoveAllBtn: "#E60000",
+    lightRemoveAllBtnColor: "#828282",
+  };
 
-  useEffect(() => {
-    api
-      .get()
-      .then((res) => {
-        setProducts(res.data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
-
-  const filteredProducts = products.filter((el) => {
-    return el.name
-      .toLocaleLowerCase()
-      .includes(search.toLocaleLowerCase().trim());
+  const invertedTheme = ({
+    lightBgPrimary,
+    darkBgPrimary,
+    lightBgSecundary,
+    darkBgSecundary,
+    lightColor,
+    darkColor,
+    lightRemoveAllBtn,
+    darkRemoveAllBtn,
+    lightBorderRemoveAllBtn,
+    darkBorderRemoveAllBtn,
+    lightRemoveAllBtnColor,
+  }) => ({
+    lightBgPrimary: darkBgPrimary,
+    darkBgPrimary: lightBgPrimary,
+    lightBgSecundary: darkBgSecundary,
+    darkBgSecundary: lightBgSecundary,
+    lightColor: darkColor,
+    darkColor: lightColor,
+    lightRemoveAllBtn: darkRemoveAllBtn,
+    darkRemoveAllBtn: lightRemoveAllBtn,
+    lightBorderRemoveAllBtn: darkBorderRemoveAllBtn,
+    darkBorderRemoveAllBtn: lightBorderRemoveAllBtn,
+    lightRemoveAllBtnColor: darkColor,
   });
 
+  const [darkmode, setDarkmode] = useState(false);
+
   return (
-    <div className="App">
-      <HamburguerHeader search={search} setSearch={setSearch} />
-      <SearchResults>
-        <Results filteredProducts={filteredProducts} search={search} />
-      </SearchResults>
-      <MainDiv>
-        <ProductCard>
-          <Products
-            products={products}
-            filteredProducts={filteredProducts}
-            cartProducts={cartProducts}
-            setCartProducts={setCartProducts}
-          />
-        </ProductCard>
-        <CartContainer>
-          <div className="header">
-            <h2>Carrinho de Compras</h2>
-          </div>
-          <div className="body">
-            <Cart
-              products={cartProducts}
-              cartProducts={cartProducts}
-              setCartProducts={setCartProducts}
-              total={total}
-              setTotal={setTotal}
-            ></Cart>
-          </div>
-        </CartContainer>
-      </MainDiv>
-    </div>
+    <ThemeProvider theme={darkmode === true ? invertedTheme(theme) : theme}>
+      <AppContainer>
+        <HamburguerHeader darkmode={darkmode} setDarkmode={setDarkmode} />
+        <SearchResults>
+          <Results />
+        </SearchResults>
+        <MainDiv>
+          <ProductCard>
+            <Products />
+          </ProductCard>
+          <Cart />
+        </MainDiv>
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
