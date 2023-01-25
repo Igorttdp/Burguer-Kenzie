4d5dbbd54eb884ad869505bc708e402df8ceec7e
Igorttdp <igorttdp.dev@email.com>
2023-01-25 15:04:50 -0300

    feat: add handleDeacrease, handleIncrease and useEffect functions
    handleDecrease and handleIncrease functions checks if newProduct exists in props, then if true, they send newProduct and setNewProduct as aditional params for contextual use. The useEffect function checks if product amount is equal to 1 for set disabled state, that is used for disable or activate decrease button

--- src/components/AmountController/index.jsx
+++ src/components/AmountController/index.jsx
@@ -1,20 +1,53 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import AmountControllerContainer from "./AmountControllerContainer";
 
-const AmountController = ({ amount }) => {
+const AmountController = ({ amount, newProduct, setNewProduct }) => {
   const { increaseProduct, decreaseProduct } = useContext(CartContext);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleDecrease = (e) => {
+    if (newProduct) {
+      decreaseProduct(e, newProduct, setNewProduct);
+    } else {
+      decreaseProduct(e);
+    }
+
+    return;
+  };
+
+  const handleIncrease = (e) => {
+    if (newProduct) {
+      increaseProduct(e, newProduct, setNewProduct);
+    } else {
+      increaseProduct(e);
+    }
+
+    return;
+  };
+
+  useEffect(() => {
+    if (newProduct && newProduct.amount === 1) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  }, [newProduct, disabled]);
 
   return (
     <AmountControllerContainer>
       <tbody>
         <tr>
           <td>
-            <button onClick={(e) => decreaseProduct(e)}>-</button>
+            <button disabled={disabled} onClick={handleDecrease}>
+              -
+            </button>
           </td>
           <td>{amount}</td>
           <td>
-            <button onClick={(e) => increaseProduct(e)}>+</button>
+            <button onClick={handleIncrease}>+</button>
           </td>
         </tr>
       </tbody>
